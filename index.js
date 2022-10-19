77ae128639e4626ed2df1295241d38e5ae9f1e40
Marvin H. Boloabio <marvinboloabio1@gmail.com>
2022-10-19 10:39:55 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+     
+    loop: true,
+
+    autoplay: {
+      delay:  3000,
+      disableOnInteraction: false,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    
+  
+    // And if we need scrollbar
+   
+  });
+
+
+  // Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+  
+    // Original:
+    // const sectionTop = current.offsetTop - 50;
+    //  
+    // Alex Turnwall's update:
+    // Updated original line (above) to use getBoundingClientRect instead of offsetTop, based on:
+    // https://plainjs.com/javascript/styles/get-the-position-of-an-element-relative-to-the-document-24/
+    // https://newbedev.com/difference-between-getboundingclientrect-top-and-offsettop
+    // This allows the use of sections inside a relative parent, which I'm not using here, but needed for a project
+    //
+    const sectionTop = (current.getBoundingClientRect().top + window.pageYOffset) - 70;
+    sectionId = current.getAttribute("id");
+    
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(" nav a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(" nav a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+}
+
+
+
+
+  
+  
